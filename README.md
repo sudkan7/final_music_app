e7532b66191db15e78ad18ec2762a5f5f0c3c8b3
sudkan7 <sudhanshukansal29@gmail.com>
2025-07-01 23:52:31 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project X MVP
+
+This is the beta release of the music-as-equity trading platform.
---
+++ app.py
@@ -0,0 +1,3 @@
+# Streamlit app entry point
+import streamlit as st
+st.title('Project X - Music as Trading MVP')
